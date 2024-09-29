2962a42d5f77b2076cf969680aa765783f8ace89
arion <53127594+JustArion@users.noreply.github.com>
2024-09-30 00:23:28 +0200

    Lower the log level by 1 for Discord logs

--- src/PlayGamesRichPresence/DiscordRichPresence/SerilogToDiscordLogger.cs
+++ src/PlayGamesRichPresence/DiscordRichPresence/SerilogToDiscordLogger.cs
@@ -11,28 +11,28 @@ public class SerilogToDiscordLogger(ILogger logger) : DiscordRPC.Logging.ILogger
     public void Trace(string message, params object[] args)
     {
         if (Level <= LogLevel.Trace)
-            logger.Verbose(message, args);
+            logger.Debug(message, args);
     }
 
     public void Info(string message, params object[] args)
     {
         if (Level <= LogLevel.Info)
-            logger.Information(message, args);
+            logger.Verbose(message, args);
     }
 
     public void Warning(string message, params object[] args)
     {
         if (Level <= LogLevel.Warning)
-            logger.Warning(message, args);
+            logger.Information(message, args);
     }
 
     public void Error(string message, params object[] args)
     {
         if (Level <= LogLevel.Error)
-            logger.Error(message, args);
+            logger.Warning(message, args);
     }
 
     public LogLevel Level { get; set; } = LogLevel.Error;
-    
+
     public static explicit operator SerilogToDiscordLogger(Logger logger) => new(logger);
 }
