43b6328dc20e0c2ca1cc71f33595c882324da73c
arion <53127594+JustArion@users.noreply.github.com>
2024-10-09 01:30:43 +0200

    Introduced first set of unit tests

---
+++ src/PlayGames_RichPresence.Tests/Unit/AppSessionMessageReaderTests.cs
@@ -0,0 +1,91 @@
+﻿namespace PlayGames_RichPresence.Tests.Unit;
+
+using Dawn.PlayGames.RichPresence.Domain;
+using Dawn.PlayGames.RichPresence.PlayGames;
+using FluentAssertions;
+
+[TestFixture(TestOf = typeof(PlayGamesAppSessionMessageReader))]
+public class AppSessionMessageReaderTests
+{
+    [SetUp]
+    public void SetUp()
+    {
+        _sut = new PlayGamesAppSessionMessageReader("Assets/Service.log");
+    }
+    private PlayGamesAppSessionMessageReader _sut;
+
+
+    [TearDown]
+    public void Cleanup()
+    {
+        _sut.Dispose();
+    }
+
+    [Test]
+    public async Task ShouldGet_FileLock()
+    {
+        await using var fileLock = _sut.AquireFileLock();
+    }
+    
+    [Test]
+    public async Task Sessions_ShouldBe_ExpectedAmount()
+    {
+        // Arrange
+        await using var fileLock = _sut.AquireFileLock();
+        
+        // Act
+        var sessionInfos = await _sut.GetAllSessionInfos(fileLock);
+
+        // Assert
+        sessionInfos.Count.Should().Be(137);
+    }
+
+    [Test]
+    public async Task FirstSession_ShouldBe_DefenseDerby()
+    {
+        // Arrange
+        await using var fileLock = _sut.AquireFileLock();
+        
+        // Act
+        var sessionInfos = await _sut.GetAllSessionInfos(fileLock);
+
+        // Assert
+        var first = sessionInfos[0];
+
+        first.Title.Should().Be("Defense Derby");
+        first.PackageName.Should().Be("com.krafton.defensederby");
+        first.AppState.Should().Be(AppSessionState.Starting);
+    }
+    
+    [Test]
+    public async Task LastSession_ShouldBe_StoppedArknights()
+    {
+        // Arrange
+        await using var fileLock = _sut.AquireFileLock();
+        
+        // Act
+        var sessionInfos = await _sut.GetAllSessionInfos(fileLock);
+
+        // Assert
+        var last = sessionInfos[^1];
+
+        last.Title.Should().Be("Arknights");
+        last.PackageName.Should().Be("com.YoStarEN.Arknights");
+        last.AppState.Should().Be(AppSessionState.Stopped);
+    }
+    
+    [Test]
+    public async Task StartingSessions_ShouldHave_NoStartTime()
+    {
+        // Arrange
+        await using var fileLock = _sut.AquireFileLock();
+        
+        // Act
+        var sessionInfos = (await _sut.GetAllSessionInfos(fileLock))
+            .Where(x => x.AppState == AppSessionState.Starting);
+
+        // Assert
+        sessionInfos.Should().AllSatisfy(x => x.StartTime.Should().BeExactly(default));
+    } 
+
+}
---
+++ src/PlayGames_RichPresence.Tests/Unit/IconScraperTests.cs
@@ -0,0 +1,27 @@
+﻿namespace PlayGames_RichPresence.Tests.Unit;
+
+using System.Web;
+using Dawn.PlayGames.RichPresence.PlayGames;
+using FluentAssertions;
+
+[TestFixture(TestOf = typeof(PlayGamesAppIconScraper))]
+public class IconScraperTests
+{
+    private static readonly string[] _appPackages = ["com.YoStarEN.Arknights", "com.krafton.defensederby"];
+    [Test]
+    public async Task ShouldScrape_Links()
+    {
+        // Act
+        foreach (var appPackage in _appPackages)
+        {
+            var link = await PlayGamesAppIconScraper.TryGetIconLinkAsync(appPackage);
+            // Assert
+
+            link.Should().NotBeNullOrEmpty();
+            
+            Uri.TryCreate(link, UriKind.Absolute, out var uri).Should().BeTrue();
+            uri!.Scheme.Should().Be("https");
+        }
+
+    }
+}
