8007c8f98dd99932b9826fdc5d94fd3c2607ec29
arion <53127594+JustArion@users.noreply.github.com>
2024-10-09 01:28:17 +0200

    Introduced a FileLock implementation for managing the lifetimes of file handles

---
+++ src/PlayGames_RichPresence/PlayGames/FileOperations/FileLock.cs
@@ -0,0 +1,32 @@
+﻿using FileAccess = System.IO.FileAccess;
+
+namespace Dawn.PlayGames.RichPresence.PlayGames.FileOperations;
+
+internal sealed class FileLock : IAsyncDisposable
+{
+    private readonly FileStream _fileLock;
+    public StreamReader Reader { get; }
+
+    private FileLock(string filePath)
+    {
+        if (!File.Exists(filePath))
+            throw new FileNotFoundException(nameof(filePath));
+
+        _fileLock = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        Reader = new StreamReader(_fileLock);
+    }
+
+    public static FileLock Aquire(string filePath, out StreamReader reader)
+    {
+        var fileLock = new FileLock(filePath);
+        reader = fileLock.Reader;
+        return fileLock;
+    }
+    public static FileLock Aquire(string filePath) => new(filePath);
+
+    public async ValueTask DisposeAsync()
+    {
+        Reader.Dispose();
+        await _fileLock.DisposeAsync();
+    }
+}
