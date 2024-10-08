301815c82b907d4c2ac226c25ea90a5f9d3a84b2
arion <53127594+JustArion@users.noreply.github.com>
2024-10-09 01:27:42 +0200

    Refactor: FileSystemWatcher is now its own dedicated class

---
+++ src/PlayGames_RichPresence/PlayGames/PlayGamesLogWatcher.cs
@@ -0,0 +1,28 @@
+﻿namespace Dawn.PlayGames.RichPresence.PlayGames;
+
+public class PlayGamesLogWatcher : IDisposable
+{
+    private readonly FileSystemWatcher _logFileWatcher;
+    public PlayGamesLogWatcher(string filePath)
+    {
+        _logFileWatcher = new();
+        _logFileWatcher.Path = Path.GetDirectoryName(filePath)!;
+        _logFileWatcher.Filter = Path.GetFileName(filePath);
+        _logFileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+        _logFileWatcher.Changed += (_, args) => FileChanged?.Invoke(this, args);
+        _logFileWatcher.Error += (_, args) => Error?.Invoke(this, args);
+    }
+
+    public event EventHandler<FileSystemEventArgs>? FileChanged;
+    public event EventHandler<ErrorEventArgs>? Error;
+
+    public void Initialize() => _logFileWatcher.EnableRaisingEvents = true;
+    public void Stop() => _logFileWatcher.EnableRaisingEvents = false;
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        _logFileWatcher.EnableRaisingEvents = false;
+        _logFileWatcher.Dispose();
+    }
+}
