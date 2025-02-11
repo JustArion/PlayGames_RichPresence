6f37a245e5dcb2bf56b44a9753df576ffaec6ad9
JustArion <53127594+JustArion@users.noreply.github.com>
2025-02-11 08:22:50 +0100

    Fatal Bugfix: Directory being null caused a fatal exception to be thrown

--- src/PlayGames_RichPresence/PlayGames/PlayGamesLogWatcher.cs
+++ src/PlayGames_RichPresence/PlayGames/PlayGamesLogWatcher.cs
@@ -2,8 +2,28 @@
 
 public class PlayGamesLogWatcher : IDisposable
 {
-    private readonly FileSystemWatcher _logFileWatcher;
+    private FileSystemWatcher? _logFileWatcher;
     public PlayGamesLogWatcher(string filePath)
+    {
+        var fi = new FileInfo(filePath);
+        if (fi.Directory is { Exists: true })
+        {
+            CreateLogWatcher(filePath);
+            return;
+        }
+
+        Log.Information("'{LogPath}' is not present, will wait for its creation via lazy initialization", filePath);
+
+        Task.Factory.StartNew(async () =>
+        {
+            while (fi.Directory is not { Exists: true })
+                await Task.Delay(TimeSpan.FromMinutes(1));
+
+            CreateLogWatcher(filePath);
+        }, TaskCreationOptions.LongRunning);
+    }
+
+    private void CreateLogWatcher(string filePath)
     {
         _logFileWatcher = new();
         _logFileWatcher.Path = Path.GetDirectoryName(filePath)!;
@@ -11,17 +31,43 @@ public class PlayGamesLogWatcher : IDisposable
         _logFileWatcher.NotifyFilter = NotifyFilters.LastWrite;
         _logFileWatcher.Changed += (_, args) => FileChanged?.Invoke(this, args);
         _logFileWatcher.Error += (_, args) => Error?.Invoke(this, args);
+
+        _logFileWatcher.EnableRaisingEvents = _shouldRaiseEvents;
     }
 
     public event EventHandler<FileSystemEventArgs>? FileChanged;
     public event EventHandler<ErrorEventArgs>? Error;
 
-    public void Initialize() => _logFileWatcher.EnableRaisingEvents = true;
-    public void Stop() => _logFileWatcher.EnableRaisingEvents = false;
+    private bool _shouldRaiseEvents;
+
+    public void Initialize()
+    {
+        if (_logFileWatcher is null)
+        {
+            _shouldRaiseEvents = true;
+            return;
+        }
+        _logFileWatcher.EnableRaisingEvents = true;
+    }
+
+    public void Stop()
+    {
+        if (_logFileWatcher is null)
+        {
+            _shouldRaiseEvents = false;
+            return;
+        }
+
+        _logFileWatcher.EnableRaisingEvents = false;
+    }
 
     public void Dispose()
     {
         GC.SuppressFinalize(this);
+
+        if (_logFileWatcher == null)
+            return;
+
         _logFileWatcher.EnableRaisingEvents = false;
         _logFileWatcher.Dispose();
     }
