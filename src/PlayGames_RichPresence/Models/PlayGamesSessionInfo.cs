3244df9677585bbff4867ae09f2fabbbea122a1f
JustArion <53127594+JustArion@users.noreply.github.com>
2025-02-15 01:07:02 +0100

    Allow Title to be mutable

--- src/PlayGames_RichPresence/Models/PlayGamesSessionInfo.cs
+++ src/PlayGames_RichPresence/Models/PlayGamesSessionInfo.cs
@@ -2,5 +2,6 @@
 
 public record PlayGamesSessionInfo(string PackageName, DateTimeOffset StartTime, string Title, AppSessionState AppState)
 {
+    public string Title { get; set; } = Title;
     public string RawText { get; init; } = "";
 }
