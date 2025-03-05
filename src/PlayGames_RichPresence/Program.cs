259d31809c56076b32d81cb45a930d2f97faa340
JustArion <53127594+JustArion@users.noreply.github.com>
2025-03-05 05:53:16 +0100

    - Bugfix: Application log file would be created in an invalid directory leading to no logs being recorded for applications started via Windows (Startup)

--- src/PlayGames_RichPresence/Program.cs
+++ src/PlayGames_RichPresence/Program.cs
@@ -26,7 +26,7 @@ internal static class Program
     private static async Task Main(string[] args)
     {
         Arguments = new(args);
-
+        Environment.CurrentDirectory = AppContext.BaseDirectory; // Startup sets it to %windir%
         ApplicationLogs.Initialize(false);
 
         if (!Arguments.NoAutoUpdate)
