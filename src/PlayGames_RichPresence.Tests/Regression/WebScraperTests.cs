221b4d9538db09d9185d9d0302a62367675331aa
arion <53127594+JustArion@users.noreply.github.com>
2025-08-28 23:41:52 +0200

    Allow tests to specify their own retry policy

--- src/PlayGames_RichPresence.Tests/Regression/WebScraperTests.cs
+++ src/PlayGames_RichPresence.Tests/Regression/WebScraperTests.cs
@@ -1,4 +1,7 @@
-﻿namespace PlayGames_RichPresence.Tests.Regression;
+﻿using Polly;
+using Polly.Retry;
+
+namespace PlayGames_RichPresence.Tests.Regression;
 
 using System.Collections.Frozen;
 using Dawn.PlayGames.RichPresence.PlayGames;
@@ -9,12 +12,16 @@ public class WebScraperTests
     private static readonly string[] _validPackages = ["com.YoStarEN.Arknights", "com.nexon.bluearchive"];
     private static readonly string[] _invalidPackages = ["com.android.vending", "com.android.browser"];
 
+    private static readonly AsyncRetryPolicy<PlayStoreWebScraper.PlayStorePackageInfo?> _noRetryPolicy = Policy<PlayStoreWebScraper.PlayStorePackageInfo?>
+        .Handle<Exception>()
+        .WaitAndRetryAsync(1, _ => TimeSpan.FromSeconds(0));
+    
     [Test]
     [TestCaseSource(nameof(_validPackages))]
     public async Task TryGetInfoAsync_WithValidAppPackage_ReturnsValidLink(string packageName)
     {
         // Act
-        var packageInfo = await PlayStoreWebScraper.TryGetPackageInfo(packageName);
+        var packageInfo = await PlayStoreWebScraper.TryGetPackageInfo(packageName, _noRetryPolicy);
 
         var link = packageInfo?.IconLink;
 
@@ -31,7 +38,7 @@ public class WebScraperTests
     public async Task TryGetInfoAsync_WithInvalidAppPackage_ReturnsNull(string packageName)
     {
         // Act
-        var packageInfo = await PlayStoreWebScraper.TryGetPackageInfo(packageName);
+        var packageInfo = await PlayStoreWebScraper.TryGetPackageInfo(packageName, _noRetryPolicy);
 
         // Assert
         packageInfo.Should().BeNull();
@@ -41,7 +48,7 @@ public class WebScraperTests
     public async Task TryGetInfoAsync_WithEmptyAppPackage_ReturnsNull()
     {
         // Act
-        var packageInfo = await PlayStoreWebScraper.TryGetPackageInfo(string.Empty);
+        var packageInfo = await PlayStoreWebScraper.TryGetPackageInfo(string.Empty, _noRetryPolicy);
             
         // Assert
         packageInfo.Should().BeNull();
--- src/PlayGames_RichPresence/PlayGames/PlayStoreWebScraper.cs
+++ src/PlayGames_RichPresence/PlayGames/PlayStoreWebScraper.cs
@@ -10,23 +10,24 @@ public static partial class PlayStoreWebScraper
 {
     public record PlayStorePackageInfo(string IconLink, string Title);
 
+    private static readonly HttpClient _client = new();
     private static readonly AsyncRetryPolicy<PlayStorePackageInfo?> _retryPolicy = Policy<PlayStorePackageInfo?>
         .Handle<Exception>()
         .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt) - 1));
 
     private static readonly ConcurrentDictionary<string, PlayStorePackageInfo> _webCache = new();
-    public static async ValueTask<PlayStorePackageInfo?> TryGetPackageInfo(string packageName)
+    public static async ValueTask<PlayStorePackageInfo?> TryGetPackageInfo(string packageName, AsyncRetryPolicy<PlayStorePackageInfo?>? retryPolicy = null)
     {
         if (_webCache.TryGetValue(packageName, out var link))
             return link;
 
+        retryPolicy ??= _retryPolicy;
+
         try
         {
-            return await _retryPolicy.ExecuteAsync(async () =>
+            return await retryPolicy.ExecuteAsync(async () =>
             {
-                using var client = new HttpClient();
-
-                var storePageContent = await client.GetStringAsync($"https://play.google.com/store/apps/details?id={packageName}");
+                var storePageContent = await _client.GetStringAsync($"https://play.google.com/store/apps/details?id={packageName}");
 
                 var match = GetImageRegex().Match(storePageContent);
 
