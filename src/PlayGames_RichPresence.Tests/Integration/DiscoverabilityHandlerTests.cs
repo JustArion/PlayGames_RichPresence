958135476253d5098366e58842260ccd75665a32
arion <53127594+JustArion@users.noreply.github.com>
2025-12-08 03:06:24 +0100

    Add a test for the Discoverability Handler

---
+++ src/PlayGames_RichPresence.Tests/Integration/DiscoverabilityHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using Dawn.PlayGames.RichPresence.Discord;
+
+namespace PlayGames_RichPresence.Tests.Integration;
+
+[TestFixture(TestOf =  typeof(DiscoverabilityHandler))]
+public class DiscoverabilityHandlerTests
+{
+    [TestCase]
+    public async Task Should_ReturnEntries()
+    {
+        // Arrange
+        var handler = new DiscoverabilityHandler();
+
+        // Act
+        var applicationId = await handler.TryGetOfficialApplicationId("Clash of Clans");
+
+        // Assert
+        applicationId
+            .Should()
+            .NotBeNullOrWhiteSpace();
+
+        handler._discoverablePresences
+            .Should()
+            .HaveCountGreaterThan(1000);
+
+        handler._discoverablePresences
+            .Should()
+            .AllSatisfy(x =>
+            {
+                x.Name
+                    .Should()
+                    .NotBeNullOrWhiteSpace();
+                
+                x.Id
+                    .Should()
+                    .NotBeNullOrWhiteSpace();
+            });
+
+    }
+}
