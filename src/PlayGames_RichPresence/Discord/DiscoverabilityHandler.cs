878014ef4dc569093dfd89e1f5c5065cf4a19148
arion <53127594+JustArion@users.noreply.github.com>
2025-12-10 08:25:09 +0100

    Add disk caching for official presence ids

--- src/PlayGames_RichPresence/Discord/DiscoverabilityHandler.cs
+++ src/PlayGames_RichPresence/Discord/DiscoverabilityHandler.cs
@@ -16,38 +16,151 @@ internal class DiscoverabilityHandler
         .WaitAndRetryAsync(MAX_RETRIES,
             _ => TimeSpan.FromSeconds(1));
 
+    private readonly SemaphoreSlim _sync = new(1, 1);
+    private bool _aquiredInitialLock;
+
     internal DiscoverableRichPresence[]? _discoverablePresences = [];
-    private readonly Task _initializationTask;
-    public DiscoverabilityHandler() =>
-        _initializationTask = Task.Run(async () =>
+    public DiscoverabilityHandler() => Task.Run(PopulateDiscoverablePresences);
+
+    private async Task PopulateDiscoverablePresences()
+    {
+        await _sync.WaitAsync();
+        _aquiredInitialLock = true;
+
+        try
         {
             try
             {
-                using var client = new HttpClient();
+                if (File.Exists("detectable.json"))
+                {
+                    Log.Debug("Found detectable.json");
 
-                _discoverablePresences = await _retryPolicy.ExecuteAsync(async ()
-                    => await client.GetFromJsonAsync<DiscoverableRichPresence[]?>("https://discord.com/api/v9/games/detectable", _options));
+                    _discoverablePresences =
+                        JsonSerializer.Deserialize<DiscoverableRichPresence[]>(
+                            await File.ReadAllTextAsync("detectable.json"), _options);
 
-                var count = _discoverablePresences?.Length ?? 0;
+                    var count = _discoverablePresences?.Length ?? 0;
+                    Log.Debug("Read from disk, found {OfficialPresenceCount} Official Presences", count);
 
-                Log.Verbose("Got {DiscoverableRichPresenceCount} Official Rich Presence Ids", count);
+                    _ = Task.Run(GetDiscoverablePresencesFromRemote).ContinueWith(async task =>
+                    {
+                        if (!task.IsCompletedSuccessfully || _discoverablePresences == null || task.Result == null)
+                            return;
+
+                        await _sync.WaitAsync();
+                        try
+                        {
+                            var remotePresences = task.Result;
+                            var localLength = _discoverablePresences.Length;
+                            var remoteLength = remotePresences.Length;
+
+                            if (localLength == remoteLength)
+                                return;
+
+                            Log.Debug("Difference detected in Official Presence Count. ({LocalLength}/{RemoteLength}) Merging Local and Remote!", localLength, remoteLength);
+
+                            // We're merging local and remote (While filtering out duplicates)
+                            // This allows us to possibly hold a larger collection of presences than remote
+                            // This is good for if remote reduces the presence count from their api (for some reason), which has happened before
+                            // And is the reason for all these checks
+                            _discoverablePresences = _discoverablePresences.Concat(remotePresences)
+                                .DistinctBy(x => x.Id)
+                                .ToArray();
+
+                            _ = Task.Run(SavePresencesToDisk);
+                        }
+                        finally
+                        {
+                            _sync.Release();
+                        }
+                    });
+
+                    return;
+                }
             }
-            catch (HttpRequestException) { }
-            catch (Exception e) when (e is not HttpRequestException)
-            {
-                Log.Error(e, "Failed to get Official Rich Presence Ids");
-            }
-        });
+            catch (JsonException) { }
+
+            _discoverablePresences = await GetDiscoverablePresencesFromRemote();
+            _ = Task.Run(SavePresencesToDisk);
+        }
+        finally
+        {
+            _sync.Release();
+        }
+    }
+
+    // This is a bit more safer than directly writing to detectable.json
+    // Since the app can exit / crash / the PC powers off during a JSON write, causing corruption
+    private async Task SavePresencesToDisk()
+    {
+        try
+        {
+            await File.WriteAllTextAsync("detectable.temp.json", JsonSerializer.Serialize(_discoverablePresences, _options));
+
+            File.Move("detectable.temp.json", "detectable.json", true);
+        }
+        finally
+        {
+            if (File.Exists("detectable.temp.json"))
+                File.Delete("detectable.temp.json");
+        }
+
+        Log.Verbose("Saved Official Presences -> detectable.json");
+    }
+
+    private static async Task<DiscoverableRichPresence[]?> GetDiscoverablePresencesFromRemote()
+    {
+        try
+        {
+            using var client = new HttpClient();
+
+            var retVal = await _retryPolicy.ExecuteAsync(async ()
+                => await client.GetFromJsonAsync<DiscoverableRichPresence[]?>(
+                    "https://discord.com/api/v9/games/detectable", _options));
+
+            var count = retVal?.Length ?? 0;
+
+            Log.Verbose("Got {DiscoverableRichPresenceCount} Official Rich Presence Ids", count);
+
+            return retVal;
+        }
+        catch (HttpRequestException) { }
+        catch (Exception e) when (e is not HttpRequestException)
+        {
+            Log.Error(e, "Failed to get Official Rich Presence Ids");
+        }
+
+        return null;
+    }
 
     public async Task<string?> TryGetOfficialApplicationId(string appName)
     {
-        await _initializationTask;
+        // There's a possibility that within the time it takes to create a task this method (TryGetOfficialApplicationId) can get called
+        // Resulting in the semaphore getting passed to this method first, instead of PopulateDiscoverablePresences.
+        // This would cause _discoverablePresences to be null for the first invocation
+        if (!_aquiredInitialLock)
+            await Task.Delay(TimeSpan.FromSeconds(1));
 
-        var appId = _discoverablePresences?.FirstOrDefault(x => x.Name == appName)?.Id;
+        if (!await _sync.WaitAsync(TimeSpan.FromSeconds(2)))
+        {
+            Log.Verbose("Abandoning wait, taking too long to get an Official App Id");
+            return null;
+        }
 
-        if (appId != null)
-            Log.Verbose("Found an Official Presence for Application {ApplicationName} ({ApplicationId})", appName, appId);
+        try
+        {
+            var appId = _discoverablePresences?.FirstOrDefault(x => x.Name == appName)?.Id;
 
-        return appId;
+            if (appId != null)
+                Log.Verbose("Found an Official Presence for Application {ApplicationName} ({ApplicationId})", appName, appId);
+            else
+                Log.Debug("Unable to find an AppId for {ApplicationName}", appName);
+
+            return appId;
+        }
+        finally
+        {
+            _sync.Release();
+        }
     }
 }
